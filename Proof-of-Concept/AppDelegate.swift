60ae95229237dd0bff85ddd0a06b416ec2d68fd1
Harold Kurth <harold.kurth@gmail.com>
2015-07-27 16:08:14 -0500

    Initial menu

--- Proof-of-Concept/AppDelegate.swift
+++ Proof-of-Concept/AppDelegate.swift
@@ -15,12 +15,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printQuote:")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func printQuote(sender: AnyObject) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        println("\(quoteText) — \(quoteAuthor)")
+    }
 
 }
 
