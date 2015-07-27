93e92a64111886cacc29a88e487dc5aed9923fe9
Harold Kurth <harold.kurth@gmail.com>
2015-07-27 12:00:41 -0500

    Initial Commit

---
+++ Proof-of-Concept/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Proof-of-Concept
+//
+//  Created by Harold Kurth on 7/27/15.
+//  Copyright (c) 2015 Harold Kurth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Proof-of-Concept/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Proof-of-Concept
+//
+//  Created by Harold Kurth on 7/27/15.
+//  Copyright (c) 2015 Harold Kurth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
